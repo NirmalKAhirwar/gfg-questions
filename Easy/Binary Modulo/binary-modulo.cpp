62b63b2f9759ea3a8bcf2300b95ec240b6efba96
Nirmal Kumar Ahirwar <53752039+NirmalKAhirwar@users.noreply.github.com>
2023-05-08 19:59:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Binary Modulo/binary-modulo.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    
+        int modulo(string s,int m)
+        {
+            //code here
+            int res = 0;
+            for (int i = 0; i < s.length(); i++)
+            {
+              res = res * 2 + (s[i] - '0');
+              res %= m;
+            }
+            return res;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        int m;
+        cin>>s>>m;
+        Solution a;
+        cout<<a.modulo(s,m)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
